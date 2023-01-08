2a2cb744a52bc26bf29e6471e701d3fe36c64d9c
Tempest20 <bajrami.damir@gmail.com>
2023-01-08 09:52:05 +0100

    smol fix

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -15,10 +15,7 @@ const Intro = () => {
 
   return (
     <div className="px-6 py-8 md:px-14 md:py-24">
-      <blockquote
-        id="fader"
-        className="rounded duration-1000 p-3 md:p-6 text-gray-300 w-full md:w-1/2 mx-auto"
-      >
+      <blockquote className="rounded duration-1000 p-3 md:p-6 text-gray-300 w-full md:w-1/2 mx-auto">
         <p className=" text-2xl font-semibold text-center mb-12">{quote}</p>
         <footer className="font-medium text-lg">- {author}</footer>
       </blockquote>
